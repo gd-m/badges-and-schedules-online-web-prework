80d3f5dd94abfbbc72fdc74435a253ca66b43215
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-02-19 21:33:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,12 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(name_array)
+  array_with_message = []
+  name_array.each do |name|
+    badge_msg = badge_maker(name)
+    array_with_message.push(badge_msg)
+  end
+  return array_with_message
+end
